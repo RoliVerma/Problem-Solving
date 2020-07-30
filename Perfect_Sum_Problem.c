31188718d2af546af28e75ed3b01492be50043ae
Roli Verma <roliivverma@gmail.com>
2020-07-31 00:27:20 +0530

    Comments Added

--- Perfect_Sum_Problem.c
+++ Perfect_Sum_Problem.c
@@ -1,4 +1,13 @@
 //Solution to GFG Perfect Sum Problem
+/* I have adopted the Dynamic Programming's tabulation approach to solve the problem in C 
+INPUTS:
+tc = total # testcases 
+n = array size 
+s = required sum
+OUTPUT:
+# subsets of input array that give the given sum s
+Note : The array sizes and datatypes are fixed as per gfg problem constraint
+*/
 
 #include <stdio.h>
 #include <string.h>
@@ -20,19 +29,19 @@ void count(int n ,  int s){
    for( int i =1 ; i <= n ; i++){
        for(  int j = 1 ; j<=s ; j++){
            
-           //arr[]>s
+           //arr[]> sub_sum
            if( arr[i-1] > s){
             t[i][j] = t[i-1][j];
            }
-            //arr[]<=s (taken & not taken)
+            //arr[]<=sub_sum (taken & not taken)
             else{
-                t[i][j] = t[i-1][j] + t[i-1][ j - arr[i-1] ];
+                t[i][j] = ( t[i-1][j] + t[i-1][ j - arr[i-1] ] ) % 1000000007;
             }
        }
    }
    
   /* 
-  // UNCOMMENT TO SEE THE 2D SUBPROBLEMS CALCULATION MATRIX
+  // UNCOMMENT TO SEE THE SUBPROBLEMS CALCULATION 2D TABULATION MATRIX
    for( int i =0 ; i <= n ; i++){
        printf("\n");
       for( int j = 0 ; j<=s ; j++){
@@ -40,18 +49,19 @@ void count(int n ,  int s){
           printf("%d\t",t[i][j]);
       }
    } */
-   printf("%d\n", t[n][s] % 1000000007);
+   printf("%d\n", t[n][s]);
 }
 
 int main() {
-    int tc , n ;
+
+    int tc , n ; 
 	scanf("%d",&tc);
 	for(int i =0 ; i< tc ; i++){
 	    scanf("%d",&n);
 	     int s;
 	    for( int j =0 ; j < n ; j++){
 	        scanf("%d",&arr[j]);
-	        //printf("%d\t",arr[j]);
+	        
 	    }
 	    scanf("%d",&s);
 	    count(n , s);
